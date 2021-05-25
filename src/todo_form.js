11d51a141db9a90918baa1683090f0ec4c749a12
Will Studabaker <will.studabaker@gmail.com>
2021-05-25 11:24:15 -0400

    add todo function prop stubbed

---
+++ src/todo_form.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+
+export default function TodoForm({ addTodo }){
+
+  return(
+    <div><button onClick={addTodo}>update</button></div>
+  )
+}
--- src/todos_container.js
+++ src/todos_container.js
@@ -4,15 +4,19 @@ import TodoForm from './todo_form'
 export default function TodosContainer(){
   const [todos, setTodos] = useState([
     {text: "make breakfast", completed: false},
-    {text: "shower", completed: false}.
+    {text: "shower", completed: false},
     {text: "apply for jobs", completed: false}
   ])
+
+  function addTodo(){
+    console.log("updating")
+  }
 
   return(
     <div>
       {todos.map((todo, index) => <div><p>{todo.text}</p>
-    <input type="checkbox" id={index} name={index}></div>)}
-      <TodoForm />
+    <input type="checkbox" id={index} name={index} /></div>)}
+      <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
