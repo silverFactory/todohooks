58e1abbded72f7e48bd5975f266b5a99200d7de6
Will Studabaker <will.studabaker@gmail.com>
2021-06-01 11:59:32 -0400

    useEffect hook displays photo from pexel API

--- src/todos_container.js
+++ src/todos_container.js
@@ -36,7 +36,10 @@ export default function TodosContainer(){
   const client = createClient('563492ad6f91700001000001c1ec9594521a495e94ece6f5f94d892e');
   const query = 'Nature';
   useEffect(() => {
-    client.photos.search({ query, per_page: 1 }).then(photos => {console.log(photos)});
+    client.photos.search({ query, per_page: 1 }).then(photos => {
+      console.log(photos)
+      setPics([photos.photos[0].src.small])
+    });
   })
 
 
@@ -48,6 +51,7 @@ export default function TodosContainer(){
                                         handleChange={handleChange}/>)}
       <TodoForm addTodo={addTodo} />
       <TodoCounter todos={todos} />
+      <img src={pics[0]} alt="some picture"/>
     </div>
   )
 }
