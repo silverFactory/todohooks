8dcc43b7b26782433f136dd58cca2f234246de6a
Will Studabaker <will.studabaker@gmail.com>
2021-06-01 11:52:43 -0400

    pexels API client added, pics variable and effect added to TodosContainer

--- src/todos_container.js
+++ src/todos_container.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoForm from './todo_form'
 import TodoCounter from './todo_counter'
 import Todo from './todo'
+import { createClient } from 'pexels';
+
 
 export default function TodosContainer(){
   const [todos, setTodos] = useState([
-    {text: "make breakfast", completed: true},
-    {text: "shower", completed: true},
-    {text: "apply for jobs", completed: true}
+    {text: "make breakfast", completed: false},
+    {text: "shower", completed: false},
+    {text: "apply for jobs", completed: false}
   ])
 
 
@@ -16,8 +18,6 @@ export default function TodosContainer(){
     setTodos([...todos, {text: todo, completed: false}])
   }
 
-  //counter wasn't updating because I wasn't using setTodos!!
-  //"counter updates but the order flips and it's off by one"
   function handleChange(text){
     //find the todo that matches the text
     let toBeUpdated = todos.find(todo => todo.text === text)
@@ -28,13 +28,17 @@ export default function TodosContainer(){
     } else {
       toBeUpdated.completed = false
     }
-    //setTodos([...todos.filter(todo => todo !== toBeUpdated), toBeUpdated])
-    //slice up to toBeUpdated
-    //insert toBeUpdated
-    //slice from toBeUpdated til end
     setTodos([...todos.slice(0, i), toBeUpdated, ...todos.slice(i + 1)])
-    console.log(todos)
   }
+
+  const [pics, setPics] = useState([])
+
+  const client = createClient('563492ad6f91700001000001c1ec9594521a495e94ece6f5f94d892e');
+  const query = 'Nature';
+  useEffect(() => {
+    client.photos.search({ query, per_page: 1 }).then(photos => {console.log(photos)});
+  })
+
 
   return(
     <div>
